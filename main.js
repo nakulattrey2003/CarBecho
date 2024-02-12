8dfc3f5ba161d4b882e32dae91f2980ceeeed64c
Nakul Attrey <nakulattrey@gmail.com>
2024-02-12 09:19:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+// Search Box
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick = () => {
+    search.classList.toggle('active');
+}
+
+// Header
+let header = document.querySelector('header');
+
+window.addEventListener('scroll' , () => {
+    header.classList.toggle('shadow', window.scrollY > 100);
+    if(window.scrollY > 100)
+    {
+        header.style.background = '#E9EDFF';
+        header.style.zIndex = '1000';
+    }
+    else
+    {
+        header.style.backgroundColor = 'transparent';
+        header.style.zIndex = 'initial';
+    }
+});
+
+// Typewriter
+const text = document.querySelector(".type1");
+
+const textLoad = () => {
+    setTimeout(() => {
+        text.textContent = "Models";
+    }, 0);
+    setTimeout(() => {
+        text.textContent = "Prices";
+    }, 4000);
+    setTimeout(() => {
+        text.textContent = "Deals";
+    }, 8000);
+    setTimeout(() => {
+        text.textContent = "Colors";
+    }, 12000);
+};
+
+textLoad();
+setInterval(textLoad,12000);
